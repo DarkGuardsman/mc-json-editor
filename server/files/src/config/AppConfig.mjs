e7d5abdaf55db2a50829dd184fd6cd5442d58224
Robin Seifert <rseifert.phone@gmail.com>
2022-05-02 03:38:17 -0400

    feat: added project config logic for type and projectId

--- server/files/src/config/AppConfig.mjs
+++ server/files/src/config/AppConfig.mjs
@@ -17,9 +17,15 @@ export async function loadConfigs() {
     //TODO load from file system
     projectList = [
         {
-            id: 0,
-            name: "ICBM",
-            path: "F:/github/1.12/ICBM-Classic"
+            id: 0, //Locally generated
+            name: "ICBM", //User defined
+            type: {
+                id: "Minecraft/Forge", //User defined, dropdown menu
+                watchFolder: "/src/main/resources", //TODO pull from data server
+                version: "1.12.2" //User defined
+            },
+            projectId: "icbmclassic", //User defined or discovered when added
+            path: "F:/github/1.12/ICBM-Classic" //User defined
         }
     ]
 }
