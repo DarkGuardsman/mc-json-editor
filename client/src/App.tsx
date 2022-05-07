2a9231565bd747cf1ba4034867664f6c2e546e0f
Robin Seifert <rseifert.phone@gmail.com>
2022-05-07 06:36:50 -0400

    feat: added editor window

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import {ApolloProvider} from "@apollo/client";
 import {apolloClient} from "./ApolloSetup";
 import FileMenu from "./components/menu/menu/FileMenu";
+import EditorWindow from "./components/editor/window/EditorWindow";
 
 function App() {
     return (
         <div className="App">
             <ApolloProvider client={apolloClient}>
                 <FileMenu/>
+                <EditorWindow/>
             </ApolloProvider>
         </div>
     );
---
+++ client/src/components/editor/window/EditorWindow.tsx
@@ -0,0 +1,9 @@
+import styles from "./EditorWindow.module.css"
+
+export default function EditorWindow(): JSX.Element {
+    return (
+        <div className={styles.div}>
+
+        </div>
+    );
+}
