ee322eec09f5a3ab9343d88c0d6f20414f40c741
Robin Seifert <rseifert.phone@gmail.com>
2022-05-07 07:07:23 -0400

    feat: created editor tabs and added file name header

--- client/src/components/editor/window/EditorWindow.tsx
+++ client/src/components/editor/window/EditorWindow.tsx
@@ -1,9 +1,56 @@
 import styles from "./EditorWindow.module.css"
+import {useState} from "react";
+import {currentFileVar} from "../../../ApolloSetup";
+
+const TAB_JSON = "json";
+const TAB_EDITOR = "editor";
 
 export default function EditorWindow(): JSX.Element {
+    const [currentTab, setCurrentTab] = useState(TAB_JSON);
+    const currentFile = currentFileVar();
+    console.log("current file", currentFile);
     return (
         <div className={styles.div}>
-
+            <h1>JSON Editor</h1>
+            <h2>{`File: ${currentFile !== undefined ? currentFile: "none"}`}</h2>
+            <div className={styles.tabs}>
+                <button
+                    className={currentTab === TAB_JSON ? styles.selected : ""}
+                    onClick={() => setCurrentTab(TAB_JSON)}
+                >
+                    JSON
+                </button>
+                <button
+                    className={currentTab === TAB_EDITOR ? styles.selected : ""}
+                    onClick={() => setCurrentTab(TAB_EDITOR)}
+                >
+                    Editor
+                </button>
+                <div className={styles.tabber}/>
+            </div>
+            <DisplayTab currentTab={currentTab} />
         </div>
     );
+}
+
+interface DisplayTabProps {
+    currentTab: string
+}
+
+function DisplayTab({currentTab}: DisplayTabProps) : JSX.Element | null {
+    if(currentTab === TAB_JSON) {
+        return (
+            <div className={styles.tab}>
+                JSON
+            </div>
+        )
+    }
+    else if(currentTab === TAB_EDITOR) {
+        return (
+            <div className={styles.tab}>
+                EDITOR
+            </div>
+        )
+    }
+    return null;
 }
