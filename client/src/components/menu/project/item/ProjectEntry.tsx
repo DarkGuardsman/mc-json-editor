b743a813e86598560e60750e993b65ad76ed6c1a
Robin Seifert <rseifert.phone@gmail.com>
2022-05-07 06:36:43 -0400

    feat: added collapse/expand logic to project entry

--- client/src/components/menu/project/item/ProjectEntry.tsx
+++ client/src/components/menu/project/item/ProjectEntry.tsx
@@ -1,5 +1,8 @@
 import ContentCategoryList from "../../category/list/ContentCategoryList";
 import styles from './ProjectEntry.module.css'
+import {useState} from "react";
+import ExpandIcon from "../../../general/expand/ExpandIcon";
+import ExpandToggle from "../../../general/expand/ExpandToggle";
 
 interface ProjectEntryProps {
     projectId: number,
@@ -7,10 +10,20 @@ interface ProjectEntryProps {
 }
 
 export default function ProjectEntry({projectId, projectName}: ProjectEntryProps): JSX.Element {
+    const [isExpanded, setIsExpanded] = useState(false);
     return (
         <div className={styles.div} id={`project-${projectId}`}>
-            <h3 className={styles.header}>{projectName}</h3>
-            <ContentCategoryList projectId={projectId}/>
+            <div className={styles.header}>
+                <ExpandIcon
+                    className={styles.button}
+                    isExpanded={isExpanded}
+                    onClick={() => setIsExpanded(!isExpanded)}
+                />
+                <h3 className={styles.header}>{projectName}</h3>
+            </div>
+            <ExpandToggle isExpanded={isExpanded}>
+                <ContentCategoryList projectId={projectId}/>
+            </ExpandToggle>
         </div>
     );
 }
